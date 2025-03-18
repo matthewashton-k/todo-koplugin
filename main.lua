b14675cc9f8fe504abb8190a7ecc46fa8ed86a5f
Matthew Ashton Knochel <ffeufhd5j@relay.firefox.com>
2025-03-18 12:50:01 -0600

    fixed refresh

--- main.lua
+++ main.lua
@@ -18,7 +18,8 @@ local _ = require("gettext")
 
 local TodoApplication = WidgetContainer:extend({
     name = "todo",
-    todos = {}, -- Data store for todo items
+    todos = {},
+    current_frame = nil,
 })
 
 function TodoApplication:init()
@@ -27,56 +28,69 @@ function TodoApplication:init()
 end
 
 function TodoApplication:loadSaved()
-    -- TODO: Implement file loading
     self.todos = {
         { text = "Sample Todo", checked = false },
         { text = "Sample Todo2", checked = false }
     }
+    logger.warn("Loaded todos: " .. #self.todos .. " items")
 end
 
 function TodoApplication:saveTodos()
-    logger.warn("saved todo")
-    -- TODO: Implement file saving
+    logger.warn("Saving todos (total: " .. #self.todos .. ")")
 end
 
 function TodoApplication:createTodoItem(todo, index)
+    logger.warn("Creating todo item #" .. index .. " with checked=" .. tostring(todo.checked))
     local check_button = CheckButton:new{
         checked = todo.checked,
         callback = function()
+            logger.warn("Checkbox clicked for item #" .. index)
             self.todos[index].checked = not self.todos[index].checked
+            logger.warn("New checked state for #" .. index .. ": " .. tostring(self.todos[index].checked))
             self:saveTodos()
+            self:refreshUI() -- Refresh UI after state change
         end,
         width = Screen:scaleBySize(30),
     }
-    
+
     local text_widget = TextWidget:new{
         text = todo.text,
         face = Font:getFace("smallinfofont"),
     }
-    
+
     return HorizontalGroup:new{
         check_button,
-        HorizontalSpan:new{ width = Screen:scaleBySize(10 )},
+        HorizontalSpan:new{ width = Screen:scaleBySize(10) },
         text_widget,
     }
 end
 
+function TodoApplication:refreshUI()
+    if self.current_frame then
+        UiManager:close(self.current_frame)
+        self.current_frame = nil
+    end
+    self:showItems()
+end
+
 function TodoApplication:showItems()
+    logger.warn("Showing todo list UI")
     local screen_width = Screen:getWidth()
     local screen_height = Screen:getHeight()
+    if self.current_frame then
+        UiManager:close(self.current_frame)
+    end
+
     local todo_list = VerticalGroup:new{
         align = "left",
         id = "todo_list",
     }
-
     for index, todo in ipairs(self.todos) do
         table.insert(todo_list, self:createTodoItem(todo, index))
     end
 
-    -- Main container with background
-    local frame = FrameContainer:new{
+    self.current_frame = FrameContainer:new{
         background = Blitbuffer.COLOR_WHITE,
-        -- radius = Size.radius.window,
         bordersize = 0,
         padding = Screen:scaleBySize(10),
         dimen = Screen:getSize(),
@@ -85,8 +99,7 @@ function TodoApplication:showItems()
             Button:new{
                 text = _("Add Todo"),
                 callback = function()
-                    -- TODO: Implement add functionality
-                    logger.warn("new todo")
+                    logger.warn("Add Todo button clicked")
                 end,
             },
             ScrollableContainer:new{
@@ -99,13 +112,15 @@ function TodoApplication:showItems()
         }
     }
 
-    UiManager:show(frame)
+    UiManager:show(self.current_frame)
+    logger.warn("UI refresh completed")
 end
 
 function TodoApplication:addToMainMenu(menu_items)
     menu_items.todo = {
         text = _("Todo App"),
         callback = function()
+            logger.warn("Todo menu item clicked")
             self:showItems()
         end
     }
